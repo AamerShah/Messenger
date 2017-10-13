f1c1f4d29349af5bf60ece74109b7e490b9c40cf
Aamer Shah <1@aamer.xyz>
2017-10-13 13:06:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<link rel="stylesheet" type="text/css" href="../remove.css">
+<title>Clip</title><style>body{padding-top: 2%;} ::selection {color:red;background:black;} ::-moz-selection {color:red;background:black;}</style><meta name="viewport" content="width=device-width, initial-scale=1.0">
+<body bgcolor=efefef link=red vlink=red><center><h3><span style="color:#9c353a">MESSENGER</span></h3><hr noshade>
+
+<iframe src="index.htm" oncontextmenu="return false" onkeydown="return false" style="display:inherit;position:relative; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:70%; border:none; margin:0; padding:0; overflow:hidden; z-index:1;" seamless>
+</iframe>
+
+<hr noshade></center><form action="index.php" method="post"><input type="text" autofocus name="clip" value="" placeholder="Type here... "><input type="submit" value="Send" onClick="this.value='Sending…';"></form></strong>
+<?php
+error_reporting(0);
+	$ip = $_SERVER['REMOTE_ADDR']; 
+$today = date("D M j Y g:i:s a T");
+$host = htmlspecialchars($_POST['clip']);
+if (strlen($host) > 10000) {echo "That definitely seems fish'e! Bye!";exit;}
+elseif (strlen($host) < 1) {echo "";exit;}
+elseif($host == "#C") { file_put_contents("index.htm", "<meta http-equiv=refresh content=2>");exit;}
+elseif($host == "#CS") { file_put_contents("index.htm", "<meta http-equiv=refresh content=2><script>function pS() {var tim; window.scrollBy(0, 10); tim = setTimeout('pS()', 1);}</script><body onload=pS()>");exit;}
+if(!empty($_POST['clip'])) {
+    $logfile = fopen('index.htm', 'a+');
+    fwrite($logfile, "\n" . "<small>" . $ip . " &raquo; " . $today . "</small> &raquo; <strong>" .  $host . "</strong><hr>");
+    fclose($logfile);
+    }//}
+else { echo 'Error while saving to Clipboard!';exit;}
+?></strong>
+
+</body>
