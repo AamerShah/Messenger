752bdd8797e88a6ea4334fc8517b08235fa42fa8
Aamer Shah <1@aamer.xyz>
2017-10-20 15:47:41 +0530

    Create clear.php

---
+++ clear.php
@@ -0,0 +1,4 @@
+<?php
+file_put_contents("index.htm", "<meta http-equiv=refresh content=0>");
+echo '<h2><center>Chat Cleared!</center></h2><meta http-equiv="refresh" content="3;URL=index.htm" />'
+?>
