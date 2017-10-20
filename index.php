fc1ee598034fccf4c7adcef8c1663e57b0af5f29
Aamer Shah <1@aamer.xyz>
2017-10-21 01:40:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <iframe name="amr" src="index.htm" oncontextmenu="return false" onkeydown="return false" style="display:inherit;position:relative; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:70%; border:none; margin:0; padding:0; overflow:hidden; z-index:1;" seamless>
 </iframe>
-<hr noshade></center><form action="index.php" method="post"><input type="text" autofocus name="clip" value="" autocomplete="off" placeholder="Type here... "><input type="submit" value="Send" onClick="this.value='Sending…';"> <a target="amr" href="clear.php" style="text-decoration:none" title="clear chat">x</a></form></strong>
+<hr noshade></center><form action="index.php" method="post"><input type="text" autofocus name="clip" value="" required autocomplete="off" placeholder="Type here... "><input type="submit" value="Send" onClick="this.value='Sending…';"> <a target="amr" href="clear.php" style="text-decoration:none" title="clear chat">x</a></form></strong>
 <?php
 error_reporting(0);
 	$ip = $_SERVER['REMOTE_ADDR']; 
